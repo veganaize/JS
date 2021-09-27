e132ed0d8babae74eebd6687614bc8aedceb5766
veganaize <veganaize@users.noreply.github.com>
2021-09-26 21:42:27 -0700

    Add description

--- DSA/1337/0001-two-sum/two-sum-brute-recursive-b.js
+++ DSA/1337/0001-two-sum/two-sum-brute-recursive-b.js
@@ -1,9 +1,13 @@
-console.log( twoSum([2,7,11,15], 9) );  // [0,1]
-console.log( twoSum([3,2,4], 6)     );  // [1,2]
-console.log( twoSum([3,3], 6)       );  // [0,1]
-console.log( twoSum([4,4,3,3], 6)   );  // [2,3]
-console.log( twoSum([6,4,4,0,3], 6) );  // [0,3]
-console.log( twoSum([1000000000,3,9,-5,-999999994], 6) );  // [0,4]
+/**
+ * Given an array of integers nums and
+ * an integer target, return indices of
+ * the two numbers such that they add up
+ * to target.
+ *
+ * You may assume that each input would
+ * have exactly one solution, and you
+ * may not use the same element twice.
+ */
 
 
 function twoSum(nums, target) {
@@ -20,3 +24,11 @@ function twoSum(nums, target) {
             arguments[3] == nums.length ? arguments[2]+2 : arguments[3]+1
     );
 }
+
+
+console.log( twoSum([2,7,11,15], 9) );  // [0,1]
+console.log( twoSum([3,2,4], 6)     );  // [1,2]
+console.log( twoSum([3,3], 6)       );  // [0,1]
+console.log( twoSum([4,4,3,3], 6)   );  // [2,3]
+console.log( twoSum([6,4,4,0,3], 6) );  // [0,3]
+console.log( twoSum([1000000000,3,9,-5,-999999994], 6) );  // [0,4]
