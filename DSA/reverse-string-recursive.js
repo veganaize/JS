d44d1fa1401ef7a2560232159e9298eb5fc194cc
veganaiZe <veganaize@users.noreply.github.com>
2025-01-31 13:18:38 -0800

    Update reverse-string-recursive.js

--- DSA/reverse-string-recursive.js
+++ DSA/reverse-string-recursive.js
@@ -4,10 +4,14 @@ function reverse(a) {
     return a[a.length-1] + reverse(a.splice(1,a.length-2)) + a[0]
 }
 
-function reverse_string(s) { return s ? reverse(s.split('')) : s }
+function reverse_string(s) { return s ? reverse(s.toString().split('')) : s }
 
-reverse_string()       // undefined
-reverse_string('')     // ""
-reverse_string('0')    // "0"
-reverse_string('01')   // "10"
-reverse_string('012')  // "210"
+reverse_string()        // undefined
+reverse_string('')      // ""
+reverse_string('0')     // "0"
+reverse_string('01')    // "10"
+reverse_string('012')   // "210"
+reverse_string('0123')  // "3210"
+reverse_string(true)    // "eurt"
+reverse_string(false)   // false
+reverse_string(-1234)   // "4321-"
