acd7b674b7ec27421de3ba05df1bcbdfff108a6d
veganaiZe <veganaize@users.noreply.github.com>
2025-01-31 11:57:48 -0800

    Update reverse-string-iterative.js

--- DSA/reverse-string-iterative.js
+++ DSA/reverse-string-iterative.js
@@ -1,6 +1,7 @@
 function reverse_string(s) {
-    var a = s.split('')
-	for (var i=0; i < a.length/2; ++i) {
+    if (! s) { return s }
+    var a = s.toString().split('')
+    for (var i=0; i < a.length/2; ++i) {
         var tmp = a[i]
         a[i] = a[a.length-1-i]
         a[a.length-1-i] = tmp
@@ -8,4 +9,12 @@ function reverse_string(s) {
     return a.join('')
 }
 
-reverse_string('hello world!')
+reverse_string()        // undefined
+reverse_string('')      // ""
+reverse_string('0')     // "0"
+reverse_string('01')    // "10"
+reverse_string('012')   // "210"
+reverse_string('0123')  // "3210"
+reverse_string(true)    // "eurt"
+reverse_string(false)   // false
+reverse_string(-1234)   // "4321-"
